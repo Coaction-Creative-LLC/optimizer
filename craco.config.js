991de6d7482d16169414ff9634af90cc68d767b8
waleed <waleedzaman7860@gmail.com>
2023-10-04 23:19:13 +0500

    craco added

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+const path = require("./static/js/main.e4f274cb.js");
+
+module.exports = {
+  webpack: {
+    configure: {
+      output: {
+        filename: "./static/js/main.e4f274cb.js",
+      },
+    },
+  },
+};
