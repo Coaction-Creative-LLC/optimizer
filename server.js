2292904c46ad2b24664020c2094441993069df4d
waleed <waleedzaman7860@gmail.com>
2023-10-05 01:21:27 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const express = require('express') 
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const app = express();
 
-const app = express() 
-const port = process.env.PORT || 3000
+app.use(express.static(path.join(__dirname, "build")));
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const PORT = process.env.PORT || 3001;
+app.use(cors());
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
