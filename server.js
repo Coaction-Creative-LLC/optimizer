b213c505b2f27ce20db6dd691c5ad1753f402451
waleed <waleedzaman7860@gmail.com>
2023-10-05 23:13:18 +0500

    add  npm version

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const path = require("path");
-const cors = require("cors");
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+// Check if request is from HTTP and if so, redirect to HTTPS
+app.enable("trust proxy");
+app.use((req, res, next) => {
+  req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
 });
 
-const PORT = process.env.PORT || 3001;
-app.use(cors());
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.use(express.json());
+
+// Your static site folder name
+app.use(express.static(path.join(__dirname, "..", "build")));
+
+// Root Redirects to the build in assets folder
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "build"));
+});
+
+// Any Page Redirects to the build in assets folder index.html that will load the react app
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "build/index.html"));
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port: ", port);
 });
