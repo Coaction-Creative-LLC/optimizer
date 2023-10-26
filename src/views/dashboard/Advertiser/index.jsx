347808de7e211d342bf52273a26866037887af6f
waleed <waleed@coactioncreative.com>
2023-10-26 21:08:12 +0500

    - removing unused imports

--- src/views/dashboard/Advertiser/index.jsx
+++ src/views/dashboard/Advertiser/index.jsx
@@ -1,15 +1,4 @@
-import {
-  Autocomplete,
-  Box,
-  InputAdornment,
-  TextField,
-  createFilterOptions,
-} from "@mui/material";
-import React from "react";
 import InnerHeader from "ui-component/InnerHeader";
-import SearchIcon from "@mui/icons-material/Search";
-import { ArrowDropDown } from "@mui/icons-material";
-import { useTheme } from "@mui/material/styles";
 import { FilterOptions } from "components/advertiser";
 
 const text = [
@@ -20,8 +9,6 @@ const text = [
 ];
 
 const MainAdvertiser = () => {
-  const theme = useTheme();
-
   return (
     <div>
       <InnerHeader title={"Advertiser"} text={text} />
